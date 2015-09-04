5553d776f10967292f3c3e4deceadad938aa9790
Bernd Wurst <bernd@bwurst.org>
2015-09-04 06:33:22 +0200

    Logfile-Support für RS232, Startscript

---
+++ rs232/bin/check.sh
@@ -0,0 +1,15 @@
+RUNNING=1
+
+if [ -f /tmp/hargassner-rs232.pid ] ; then
+  PID="$(</tmp/hargassner-rs232.pid)"
+  if ! [ -d "/proc/$PID" ] ; then
+    RUNNING=0
+  fi
+else
+  RUNNING=0
+fi
+
+if [ "$RUNNING" == 0 ] ; then
+  echo 'Hargassner-Monitor laeuft nicht, wird gestartet...'
+  cd ~/hargassner-monitor/rs232/ ; screen -dmS rs232 ./bin/rs232monitor --rrd
+fi
