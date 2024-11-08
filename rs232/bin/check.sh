c37a90d5604d4a2e02f4b268929ec3951edf4ace
Bernd Wurst <bernd@bwurst.org>
2024-11-08 07:33:57 +0100

    raspi zero anpassungen

--- rs232/bin/check.sh
+++ rs232/bin/check.sh
@@ -12,5 +12,5 @@ fi
 
 if [ "$RUNNING" == 0 ] ; then
   echo 'Hargassner-Monitor laeuft nicht, wird gestartet...'
-  cd ~/hargassner-monitor/rs232/ ; screen -dmS rs232 ./bin/rs232monitor --rrd
+  cd ~/hargassner-monitor/rs232/ ; screen -dmS rs232 -h 1000 ./bin/rs232monitor
 fi
